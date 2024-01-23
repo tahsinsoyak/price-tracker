dbf2ea3c8605a02f281efa021eb09e4f343098fe
tahsinsoyak <tahsinsoyakk@gmail.com>
2024-01-23 09:59:41 +0300

    Update amazon_scraper.py

--- app/scrapers/amazon_scraper.py
+++ app/scrapers/amazon_scraper.py
@@ -21,36 +21,53 @@ def amazon_scraper(product_url):
         # Gerekli verileri çek ve düzenle
         title = soup.find('span', {'id': 'productTitle'}).get_text().strip()
         price = soup.find('span', {'class': 'a-offscreen'}).get_text().strip()
-        images = []
 
         # Sayfadaki tüm ürün resimlerini bul ve listeye ekle
-        li_elements = soup.find_all('li', {'class': 'image'})
-        for li_element in li_elements:
-            # li öğesi içinde img etiketini bul
-            img = li_element.find('img')
+        image_elemnt = soup.find('div', {'id': 'imageBlock_feature_div'})
+        images = []
+        if image_elemnt:
+            img_elements = image_elemnt.find_all('img')
+            
+            for img in img_elements:
+                # Eğer img etiketi bulunduysa ve 'src' özelliği varsa
+                if 'src' in img.attrs:
+                    src = img['src']
+                    images.append(src)
 
-            # Eğer img etiketi bulunduysa ve 'src' özelliği varsa
-            if img and 'src' in img.attrs:
-                src = img['src']
-                images.append(src)
+        # Process the images
+        processed_images = []
 
+        for original_image in images:
+            # Find the index of "_AC_" in the URL
+            index_of_ac = original_image.find("_AC_")
+            
+            if index_of_ac != -1:
+                # Remove everything after "_AC_" and add ".jpg"
+                processed_image = original_image[:index_of_ac] + "jpg"
+                processed_images.append(processed_image)
+            else:
+                processed_images.append(original_image)
+
+        images = processed_images
         # Ürünün teknik özelliklerini içeren tabloyu bul
-        tech_spec_table = soup.find('table', {'id': 'productDetails_techSpec_section_1'})
+        tech_spec_table = soup.find('table', {'class': 'prodDetTable'})
+        print(tech_spec_table)
 
         # Eğer tablo bulunduysa
         if tech_spec_table:
             tech_spec_data = ""
 
             # Tablodaki her satır için
-            for row in tech_spec_table.find_all('tr'):
-                key = row.find(['th']).get_text().strip()
-                value = row.find(['td']).get_text().strip()
+            for row in tech_spec_table[0].find_all('tr'):
+                key = row.find('th').get_text().strip()
+                value = row.find('td').get_text().strip()
                 tech_spec_data += f"{key}: {value}<br>"
         else:
             # Eğer tablo bulunamazsa
             tech_spec_data = None
             print("Teknik özellik bölümü sayfada bulunamadı.")
 
+        
         # Elde edilen verileri sözlük olarak döndür
         return {'url': product_url,'title': title, 'price': price, 'images': images, 'tech_spec_data': tech_spec_data}
     else:
